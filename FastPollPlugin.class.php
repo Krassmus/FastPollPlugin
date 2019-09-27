5147ab726b15d28376c9e1e45437f89704bd8739
Rasmus Fuhse <fuhse@data-quest.de>
2019-09-27 11:09:41 +0200

    change navigation

--- FastPollPlugin.class.php
+++ FastPollPlugin.class.php
@@ -10,21 +10,16 @@ class FastPollPlugin extends StudIPPlugin implements SystemPlugin {
     {
         parent::__construct();
         if ($this->isAdmin()) {
-            if (Navigation::hasItem("/start/serienbriefe")) {
+            if (Navigation::hasItem("/messaging")) {
                 $tab = new AutoNavigation(_("Schnellumfragen"), PluginEngine::getURL($this, array(), "polls/index"));
-                Navigation::addItem("/serienbriefe/fastpoll", $tab);
-                $tab = new Navigation(_("Schnellumfragen"), PluginEngine::getURL($this, array(), "polls/index"));
-                Navigation::addItem("/start/serienbriefe/fastpoll", $tab);
-            } else {
-                $tab = new AutoNavigation(_("Schnellumfragen"), PluginEngine::getURL($this, array(), "polls/index"));
-                Navigation::addItem("/tools/fastpoll", $tab);
+                Navigation::addItem("/messaging/fastpoll", $tab);
             }
         }
     }
 
     public function isAdmin()
     {
-        return $GLOBALS['perm']->have_perm('root')
+        return $GLOBALS['perm']->have_perm('dozent')
             || RolePersistence::isAssignedRole($GLOBALS['user']->id, 'Prüfungsamt');
     }
 }
